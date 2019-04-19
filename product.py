f9d9295310eff4705cd1cd999dd3ed14631e98cf
Sujay Vadlakonda <37093385+Rubiks2002@users.noreply.github.com>
2019-04-18 19:41:11 -0500

    Create product.py

---
+++ product.py
@@ -0,0 +1,18 @@
+class Product(object):
+    def __init__(self, title, price, img_src, url, school, category):
+        self.title = title
+        self.price = price
+        self.img_src = img_src
+        self.url = url
+        self.school = school
+        self.category = category
+
+    def json(self):
+        return {'title': self.title,
+                'price': self.price,
+                'img_src': self.img_src,
+                'url': self.url,
+                'category': self.category}
+
+    def __repr__(self):
+        return 'Title: ' + self.title + ' Price: ' + self.price + ' Img_Src: ' + self.img_src + ' URL: ' + self.url + ' School: ' + self.school + ' Category: ' + self.category
